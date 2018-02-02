dc01585d117e1e2711968510655cbc0da51822fc
david.abayon <david.abayon@pointwest.com.ph>
2018-02-02 15:21:40 +0800

    Added a string

--- src/com/java/animal/shelter/main/Main.java
+++ src/com/java/animal/shelter/main/Main.java
@@ -7,7 +7,8 @@ public class Main {
 	public static void main(String[] args) {
 		AnimalShelterUI animalShelterUI = new AnimalShelterUI();
 		animalShelterUI.homePageUi();
-		
+
+		String name = "THIS IS DAVID";
 	}
 
 }
